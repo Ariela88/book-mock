6648f40abf0ccec290ea4f666d759ad4a93634af
Ariela88 <135212709+Ariela88@users.noreply.github.com>
2023-07-17 13:25:09 +0200

    first commit

---
+++ models/book.js
@@ -0,0 +1,21 @@
+class Book{
+
+
+    constructor(title,author,dop,genre,cover){
+    this.title = title
+    this.author = author
+    this.dop = dop 
+    this.genre = genre 
+    this.cover = cover
+}
+
+
+
+
+
+
+
+
+
+
+}
